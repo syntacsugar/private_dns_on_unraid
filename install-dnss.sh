c1a3e2ad523b57f3d4ddbfcf3e9a1019fb9f38ba
syntacsugar <48946999+syntacsugar@users.noreply.github.com>
2019-07-02 14:47:56 +1000

    Update install-dnss.sh

--- install-dnss.sh
+++ install-dnss.sh
@@ -1,37 +1,28 @@
-#!/bin/sh
-
 dnsDir="/etc/dns"
 dnsTar="/etc/dns/DnsServerPortable.tar.gz"
 dnsUrl="https://download.technitium.com/dns/DnsServerPortable.tar.gz"
-
 mkdir -p $dnsDir
 installLog="$dnsDir/install.log"
 echo "" > $installLog
-
 echo ""
 echo "==============================="
 echo "Technitium DNS Server Installer"
 echo "==============================="
 echo ""
-
 if [ -f "/usr/bin/dotnet" ]
 then
 	echo ".NET Core Runtime was found installed."
 else
 	echo "Installing .NET Core Runtime..."
-
 	if wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -r -s)/packages-microsoft-prod.deb" -O "$dnsDir/packages-microsoft-prod.deb"
 	then
 		dpkg -i "$dnsDir/packages-microsoft-prod.deb" >> $installLog 2>&1
-
 		add-apt-repository universe >> $installLog 2>&1 
-
 		until apt-get -y update >> $installLog 2>&1 && apt-get -y install libunwind8 icu-devtools apt-transport-https aspnetcore-runtime-2.2 >> $installLog 2>&1
 		do
 			echo "Trying again.."
 			sleep 2
 		done
-
 		echo ".NET Core Runtime was installed succesfully."
 	else
 		echo ""
@@ -39,10 +30,8 @@ else
 		exit 1
 	fi
 fi
-
 echo ""
 echo "Downloading Technitium DNS Server..."
-
 if wget -q "$dnsUrl" -O $dnsTar
 then
 	if [ -f "/etc/dns/DnsServerApp.dll" ]
